01734d37308805b07caf5d23743055e31beca894
Amanda Kirchner <geeky.brewster@gmail.com>
2017-09-20 19:54:41 -0500

    Added Angular routing to links and ng-include to index.html. Intial color schemes in css

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,21 @@
 console.log('Up and Atom!');
+var myApp = angular.module('myApp', ['ngRoute']);
+
+/// Routes ///
+myApp.config(function($routeProvider) {
+  console.log('myApp -- config');
+  $routeProvider
+    .when('/home', {
+      templateUrl: '/views/index.html',
+    })
+    .when('/aboutme', {
+      templateUrl: '/views/templates/aboutme.html',
+    })
+    .when('/projects', {
+      templateUrl: '/views/templates/projects.html',
+    })
+    .otherwise({
+      redirectTo: 'home'
+    });
+
+}); // END OF Routes config
